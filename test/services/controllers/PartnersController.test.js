0e40e8a199f471f4f37ddeb45227021e07023d4c
Magdiel Hermosillo Grande <magdielhgra@gmail.com>
2022-04-29 14:23:50 -0500

    6: Se corrigieron errores de pruebas y de los metodos en partnersController y test de partnerscontroller

--- test/services/controllers/PartnersController.test.js
+++ test/services/controllers/PartnersController.test.js
@@ -3,20 +3,17 @@ const Reader = require("./../../../lib/utils/Reader");
 
 describe("Tests para VisualThinking", () => {
     test("1: Obtener todos los partners ", () => {
-        const partners = Reader.readJsonFile("visualpartners.json")
-        const list_partners = PartnersController.getPartnersAll(partners);
+        const list_partners = PartnersController.getPartnersAll();
         expect(list_partners.length).toBe(51);
     });
 
     test("2: Obtener todos los partners emails que tienen certificacion ", () => {
-        const partners = Reader.readJsonFile("visualpartners.json")
-        const list_partners_Emails = PartnersController.getPartnersEmail(partners);
+        const list_partners_Emails = PartnersController.getPartnersEmail();
         expect(list_partners_Emails.length).toBe(29);
     });
 
     test("3: Obtener todos los partners con credito mayor ", () => {
-        const partners = Reader.readJsonFile("visualpartners.json")
-        const list_partners_credit = PartnersController.getPartnersCredit(partners)
+        const list_partners_credit = PartnersController.getPartnersCredit()
         expect(list_partners_credit.length).toBe(27);
         expect(list_partners_credit).toContain("Warren")
     });
